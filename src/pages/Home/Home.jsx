e4a64dce91b10e41d9d6a08b2be917668abfb0cd
J4ckIsB4ck <sans15615@gmail.com>
2024-03-25 16:56:05 +0200

    Added blur to all page except popup-select

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import Navbar from "../../components/Navbar";
 import "./Home.css";
 
@@ -7,40 +7,44 @@ function Home() {
     const startBtn = document.querySelector('.start-btn');
     const popupSelect = document.querySelector('.popup-select');
     const exitBtn = document.querySelector('.exit-btn');
+    const main = document.querySelector('.main-home');
 
 
     if (startBtn && popupSelect && exitBtn) {
       startBtn.onclick = () => {
         popupSelect.classList.add('active');
+        main.classList.add('active');
       }
 
       exitBtn.onclick = () => {
         popupSelect.classList.remove('active');
+        main.classList.remove('active');
       }
     }
   }, []);
 
   return (
-    <>
-    <Navbar/>
-      <section className="section-home">
-        <div className="box-home">
-          <h1>QuickB Website</h1>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid nemo beatae assumenda harum doloribus,
-            ipsum fugit rem eligendi quam soluta animi enim? Amet expedita enim maiores voluptate delectus alias dolorum?</p>
-          <button className="start-btn">Start Quiz</button>
+    <Fragment>
+        <div className="main-home">
+         <Navbar/>
+          <section className="section-home">
+                <div className="div-home">
+                  <h1>QuickB Website</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit</p>
+                  <button className="start-btn">Start Quiz</button>
+                </div>
+          </section>
         </div>
-      </section>
 
-      <div className="popup-select">
-        <h2>Select Quiz</h2>
-          <span className="info">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis magni tenetur magnam, quos corrupti numquam?</span>
-        <div className="btn-group">
-          <button className="info-btn exit-btn">Exit</button>
-          <a href="#" className="info-btn continue-btn">Continue</a>
+        <div className="popup-select">
+          <h2>Select Quiz</h2>
+            <span className="info">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis magni tenetur magnam, quos corrupti numquam?</span>
+          <div className="btn-group">
+            <button className="info-btn exit-btn">Exit</button>
+            <a href="#" className="info-btn continue-btn">Continue</a>
+          </div>
         </div>
-      </div>
-    </>
+    </Fragment>
   );
 }
 
