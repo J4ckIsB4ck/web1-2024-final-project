765fe8403b35d2c692fcc7b903b6e7ce1d3d32f4
J4ckIsB4ck <sans15615@gmail.com>
2024-04-10 21:55:57 +0300

    Carousel finished

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,12 +3,13 @@ import './Carousel.css';
 
 function Carousel() {
     useEffect(() => {
+        const wrapper = document.querySelector(".wrapper");
         const carousel = document.querySelector(".carousel");
         const arrowBtns = document.querySelectorAll(".wrapper i");
         const firstCardWidth = carousel.querySelector(".card").offsetWidth;
         const carouselChildrens = [...carousel.children];
 
-        let isDragging = false, startX, startScrollLeft;
+        let isDragging = false, startX, startScrollLeft, timeoutId;
  
         let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
 
@@ -43,29 +44,49 @@ function Carousel() {
             carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
         }
 
+        const autoPlay = () => {
+            if(window.innerWidth < 800) return;
+            timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
+        }
+        autoPlay();
+
         const infiniteScroll = () => {
             if(carousel.scrollLeft === 0){
+                carousel.classList.add("no-transition");
                 carousel.scrollLeft = carousel.scrollWidth - ( 2 * carousel.offWidth);
+                carousel.classList.remove("no-transition");
             }
             else if (Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth){
+                carousel.classList.add("no-transition");
                 carousel.scrollLeft = carousel.offsetWidth;
+                carousel.classList.remove("no-transition");
             }
+            
+            clearTimeout(timeoutId);
+            if (!wrapper.matches(":hover")) autoPlay();
         }
 
         carousel.addEventListener("mousedown", dragStart);
         document.addEventListener("mouseup", dragStop);
         carousel.addEventListener("mousemove", dragging);
+        wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+        wrapper.addEventListener("mouseleave", autoPlay);
         carousel.addEventListener("scroll", infiniteScroll);
 
         return () => {
             carousel.removeEventListener("mousedown", dragStart);
             document.removeEventListener("mouseup", dragStop);
             carousel.removeEventListener("mousemove", dragging);
+            wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+            wrapper.addEventListener("mouseleave", autoPlay);
             carousel.addEventListener("scroll", infiniteScroll);
         };
     }, []);
     return(
         <>
+        <div className='card-header'>
+            <h1>Quizzes get serious results...</h1>
+        </div>
         <div className='card-body'>
             <div className='wrapper'>
              <i id="left" className="fa-solid fa-angle-left"></i>
